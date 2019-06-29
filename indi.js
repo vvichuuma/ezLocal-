bec214d18059c5431eaa6dbae40092c7bf92caea
VISHNU <vvichugit@gmail.com>
2019-06-29 12:35:40 -0500

    added the navigation bar on the Top

---
+++ indi.js
@@ -0,0 +1,16 @@
+function displayMenu(){
+ 
+  var x = document.getElementById("mobileMenu");
+
+   console.log(x);
+
+if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.classList.add("code");  
+    x.style.display = "block";
+  }
+
+
+
+}
