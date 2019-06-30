f33720c6a281470c482a4244fbd0d9da6b83dbf4
VISHNU <vvichugit@gmail.com>
2019-06-30 08:43:03 -0500

    final commit

---
+++ about.js
@@ -0,0 +1,19 @@
+
+
+
+function displayMenu(){
+ 
+    var x = document.getElementById("mobileMenu");
+  
+     console.log(x);
+  
+  if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.classList.add("code");  
+      x.style.display = "block";
+    }
+  
+  
+  
+  }
